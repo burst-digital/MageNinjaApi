6f4c7da8b52a1965c2255539c1e1570e2329683c
Daniël <daniel@burst-digital.com>
2017-12-06 12:48:36 +0100

    Revert src/ structure

--- Observer/ProductSaveAfterObserver.php
+++ Observer/ProductSaveAfterObserver.php
@@ -33,8 +33,10 @@ class ProductSaveAfterObserver implements ObserverInterface {
     /** @var \Magento\Catalog\Model\Product $_product */
     $_product = $observer->getEvent()->getProduct();
 
-    $this->logger->debug($_product->getId());
+    $this->logger->debug('Observer updating product ' . $_product->getId() . ' in Drupal.');
 
     $client = new Client();
-
+    $result = $client->get('http://dmr.drupal/mage_ninja/import/' . $_product->getId());
+    $this->logger->debug('Status code: ' . $result->getStatusCode());
+    $this->logger->debug('Result: ' . $result->getBody());
 }}
