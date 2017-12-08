f379003a5a3bbca18b369aecb00de300f2027601
Daniël <daniel@burst-digital.com>
2017-12-08 15:52:33 +0100

    Update API url

--- Observer/ProductSaveAfterObserver.php
+++ Observer/ProductSaveAfterObserver.php
@@ -36,7 +36,7 @@ class ProductSaveAfterObserver implements ObserverInterface {
     $this->logger->debug('Observer updating product ' . $_product->getId() . ' in Drupal.');
 
     $client = new Client();
-    $result = $client->get('http://dmr.drupal/mage_ninja/import/' . $_product->getId());
+    $result = $client->get('webserver.drupal/mage_ninja/import/' . $_product->getId());
     $this->logger->debug('Status code: ' . $result->getStatusCode());
     $this->logger->debug('Result: ' . $result->getBody());
 }}
